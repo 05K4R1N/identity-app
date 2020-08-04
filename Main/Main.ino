0d747bd1e5aa5309cac5e2513800b9fef89eeaec
05K4R1N <oskr.cbba2010@gmail.com>
2020-08-03 23:19:31 -0400

    Working on libraries

--- Main/Main.ino
+++ Main/Main.ino
@@ -3,23 +3,22 @@
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
 void setup(void)
 {
-    //Set font.
     u8g.setFont(u8g_font_unifont);
 }
 void loop(void)
 {
     u8g.firstPage();
     do {
-        u8g.drawStr(2,15,"Vel = ");
         draw();
     } while (u8g.nextPage());
-    //Delay before repeating the loop.
     delay(3000);
 }
 void draw(void)
 {
-    
+    u8g.setPrintPos(2, 15);
+    u8g.print("Vel =");
     u8g.setPrintPos(55, 15);
     u8g.print(0);
-    u8g.drawStr(85, 15, "km/h");
+    u8g.setPrintPos(88, 15);
+    u8g.print("km/h");
 }
