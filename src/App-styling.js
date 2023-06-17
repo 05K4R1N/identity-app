b2793965069e3a568b9ca65f85e125c7f87258c9
05K4R1N <oskr.cbba2010@gmail.com>
2023-06-17 12:37:29 -0400

    [A] Adding microphone funcionality

---
+++ src/App-styling.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Microphone = styled.div`
+    background-color: #000;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+
+    & button.mic{
+        margin: 40px;
+        background: red;
+    }
+    & .mic .icon{
+        font-size: 12rem;
+        color: #fff;
+    }
+    & .transcription {
+        margin: 40px;
+        color: red;
+        font-size: 3rem;
+    }
+`
--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Microphone } from './App-styling';
+import IconButton from '@mui/material/IconButton';
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/MicOff';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const App = () =>  {
+  
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+  let error = (!browserSupportsSpeechRecognition) ? <h3>Browser no apto para la webapp.</h3> : null;
+  const Icon = listening ? MicIcon : MicOffIcon;
   return (
-    <div>
-      <h1>Test!</h1>
-    </div>
+    <Microphone>
+      <IconButton 
+        className="mic"
+        onMouseDown={SpeechRecognition.startListening}
+        onMouseUp={SpeechRecognition.stopListening} 
+      >
+        <Icon className="icon"/>
+      </IconButton>
+      <div className='transcription'>
+        {transcript}
+      </div>
+      {error}
+    </Microphone>
   );
 }
 
